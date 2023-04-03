47fbe7085f86cf1197ac8641240cb6f811e0dc7e
unknown <Bollingerr9@gmail.com>
2023-04-02 21:16:28 -0400

    tweaks and bug fixing

--- flask_app/controllers/project_controller.py
+++ flask_app/controllers/project_controller.py
@@ -5,7 +5,7 @@ from flask_app.models.part_model import Part
 
 @app.route('/create_project', methods=["POST"])
 def create_project():
-    if not Project.validations():
+    if 'id' not in session:
         return redirect('/create_project')
     else:
         data = {
@@ -13,6 +13,8 @@ def create_project():
             'description' : request.form['description'],
             'users_id' : session['id']
         }
+        if Project.validations(data) == False:
+            return redirect ('/create_project')
         project_id = Project.create_project(data)
         print(project_id)
         return redirect(f'/edit_project/{project_id}')
